847af10fea7884eaf4f52d7ba7d5c211dee486ab
mrskribbs <mrskribbs@gmail.com>
2022-01-27 13:29:29 -0500

    minor adustments

--- pages/MainNav.tsx
+++ pages/MainNav.tsx
@@ -3,18 +3,20 @@ import styles from '../styles/Home.module.scss'
 
 const MainNav: NextPage = () => {
 	return (
-		<div className="main-nav">
-			<img className="logo" src="assets/logo.svg" />
-			<div className="socials">
-				<a href="#">
-					<img className="icon" src="/assets/icon.svg" />
-				</a>
-				<a href="#">
-					<img className="icon" src="/assets/discord.svg" />
-				</a>
-				<a href="#">
-					<img className="icon" src="/assets/twitter.svg" />
-				</a>
+		<div className="main-nav-wrapper">
+			<div className="main-nav">
+				<img className="logo" src="assets/logo.svg" />
+				<div className="socials">
+					<a href="#">
+						<img className="icon" src="/assets/icon.svg" />
+					</a>
+					<a href="#">
+						<img className="icon" src="/assets/discord.svg" />
+					</a>
+					<a href="#">
+						<img className="icon" src="/assets/twitter.svg" />
+					</a>
+				</div>
 			</div>
 		</div>
 	)
